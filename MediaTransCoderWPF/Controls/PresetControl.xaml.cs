983c9c998c35e9be867e726ee40e7899d3b1d33a
Błażej Janus <blazejjanus@gmail.com>
2023-01-11 20:50:20 +0100

    PresetControl layout

---
+++ MediaTransCoderWPF/Controls/PresetControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaTransCoder.WPF.Controls {
+    /// <summary>
+    /// Interaction logic for VideoPresetControl.xaml
+    /// </summary>
+    public partial class PresetControl : UserControl {
+        public PresetControl() {
+            InitializeComponent();
+        }
+
+        private void purposeInput_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+
+        private void presetCheck_Checked(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void presetCheck_Unchecked(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void qualityInput_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+    }
+}
